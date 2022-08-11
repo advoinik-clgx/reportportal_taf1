4dd2647ad883b7b0708d978d3a4bfd5e22ed5101
dyadko <dyadko@i.ua>
2022-08-12 00:32:09 +0300

    api_user

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -1,7 +1,7 @@
 package configuration;
 
 public class Config {
-    public static final String BASE_URL = System.getProperty("rp.endpoint", "http://localhost:8080/");
+    public static final String BASE_URL = System.getProperty("rp_endpoint", "http://localhost:8080/");
     public static final String DRIVER_NAME = System.getProperty("web_driver");
     public static final String API_USER = System.getenv("rp_api_user");
     public static final String API_PASSWORD = System.getenv("rp_api_password");
